1376a679850d6a90ddb4ba6c797d01a68f2190fd
root <jprobeweb@gmail.com>
2022-03-10 18:36:46 -0800

    2st

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0 when success
+ */
+
+int main(void) /*Entry point */
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0); /* Return 0 when success */
+}
