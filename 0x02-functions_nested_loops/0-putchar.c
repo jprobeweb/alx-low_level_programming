536003119d7b08732320859111a8c6f8a41c16d1
root <jprobeweb@gmail.com>
2022-03-15 18:49:21 -0700

    1st

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,13 +8,13 @@
 
 int main(void)
 {
-        int i;
-        char p[] = "_putchar";
+	int i;
+	char p[] = "_putchar";
 
-        for (i = 0; i < 9; i++)
-                {
-                _putchar(p[i]);
-                }
-                _putchar('\n');
-        return (0);
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(p[i]);
+	}
+	_putchar('\n');
+	return (0);
 }
