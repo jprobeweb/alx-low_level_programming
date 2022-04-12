9c224d81a330d872aedd4c2df5f8944133e5ace5
root <jprobeweb@gmail.com>
2022-04-11 19:31:48 -0700

    1st

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
+/*
+ * File: dog.h
+ * Auth: Jordy Minka
+ * Desc: Header file that defines a new type struct dog.
+ */
+
 /**
  * struct dog - A new type describing a dog.
  * @name: The name of the dog.
